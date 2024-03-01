8a4cc3f54f56a53d9331217c37e3e130272e4a0d
Sujal Sharma <110303512+Sujal-3012@users.noreply.github.com>
2024-03-01 11:06:19 +0530

    creating exp5and6python.py

---
+++ exp5and6python.py
@@ -0,0 +1,14 @@
+#exp 5
+s = input("Enter a string : ")
+str =  ""
+for i in s :
+    str = i+str
+    
+print("Reversed string is : " , str)
+
+#exp 6
+def areaCircle(float radius) : 
+    return 3.14 * r * r
+
+a = float(input("Enter the radius of circle : "))
+print("Area is : " , areaCircle(a))
